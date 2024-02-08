05b1f7d5b8bca98cb652400cf79758735d7be9c1
Samuel <109113326+samuxka@users.noreply.github.com>
2024-02-08 18:19:06 -0300

    up

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <header>
+        <a href="#" className="logo">MyBlog</a>
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
+      </header>
+
+
+    </main>
+  );
+}
+
+export default App;
